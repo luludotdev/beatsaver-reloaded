51de91fde52102b5cb3de09a85d763dd90e6b2ed
Jack Baron <jackmbaron@gmail.com>
2019-05-17 11:12:30 +0100

    add api route to list songs

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,13 @@
 import Router from 'koa-router'
+import Song from '../mongo/models/Song'
 
 const router = new Router({
   prefix: '/api/v1.0',
+})
+
+router.get('/songs', async ctx => {
+  const songs = await Song.find({})
+  return (ctx.body = songs)
 })
 
 export { router as apiRouter }
