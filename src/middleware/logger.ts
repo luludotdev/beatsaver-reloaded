0502bc1120f99c41d44543a82c94eb8ccbf98132
Jack Baron <jackmbaron@gmail.com>
2019-05-18 08:10:34 +0100

    update logger format

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -9,10 +9,11 @@ export const logger: Middleware = async (ctx, next) => {
 
   const httpVersion = `${ctx.req.httpVersionMajor}.${ctx.req.httpVersionMinor}`
 
+  const responseLength = ctx.status === 204 ? 0 : ctx.response.length || -1
   const reqInfo = `${ctx.method} ${ctx.url} HTTP/${httpVersion}`
-  const resInfo = `${ctx.status} ${ctx.response.length || -1}`
-  const referrer = ctx.headers.referer || ctx.headers.referrer || ''
-  const ua = ctx.headers['user-agent'] || ''
+  const resInfo = `${ctx.status} ${responseLength}`
+  const referrer = ctx.headers.referer || ctx.headers.referrer || '-'
+  const ua = ctx.headers['user-agent'] || '-'
   const headers = `"${referrer}" "${ua}"`
 
   signale.info(`${ip} - "${reqInfo}" ${resInfo} ${headers}`)
