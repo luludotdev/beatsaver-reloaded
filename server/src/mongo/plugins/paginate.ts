3bcfbbfc089c52a2dac3b81b3f6bc57d7a86847a
Jack Baron <jackmbaron@gmail.com>
2019-05-18 13:58:21 +0100

    augment paginator function

---
+++ server/src/mongo/plugins/paginate.ts
@@ -0,0 +1,49 @@
+import { Document, PaginateModel, PaginateOptions } from 'mongoose'
+import paginate from 'mongoose-paginate-v2'
+import { RESULTS_PER_PAGE } from '../../env'
+
+const pOptions: PaginateOptions = {
+  limit: RESULTS_PER_PAGE,
+}
+
+interface IPaginateResult<T extends Document> {
+  docs: T[]
+  totalDocs: number
+
+  lastPage: number
+  prevPage: number | null
+  nextPage: number | null
+}
+
+const paginateFn: <D extends Document, M extends PaginateModel<D>>(
+  model: M,
+  query?: object,
+  options?: PaginateOptions
+) => Promise<IPaginateResult<D>> = async (model, query, options) => {
+  const opts: PaginateOptions = options || {}
+  const page = opts.page !== undefined ? opts.page + 1 : undefined
+
+  const {
+    docs,
+    totalDocs: td,
+    totalPages: tp,
+    prevPage: p,
+    nextPage: n,
+  } = await model.paginate(query, { ...opts, page })
+
+  const totalDocs = td as number
+  const totalPages = tp as number
+  const prev = p as number | null
+  const next = n as number | null
+
+  const lastPage = totalPages - 1
+  const prevPage = prev === null ? null : prev - 1
+  const nextPage = next === null ? null : next - 1
+
+  return { docs, totalDocs, lastPage, prevPage, nextPage }
+}
+
+export { paginateFn as paginate }
+
+// @ts-ignore
+paginate.paginate.options = pOptions
