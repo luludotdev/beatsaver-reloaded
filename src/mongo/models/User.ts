454c16181371a73a4b7b6234c5579c3badf552f7
Jack Baron <jackmbaron@gmail.com>
2019-05-18 03:17:31 +0100

    update user model

--- src/mongo/models/User.ts
+++ src/mongo/models/User.ts
@@ -1,16 +1,33 @@
 import mongoose, { Document, Schema } from 'mongoose'
-import withoutVersionKey from '../plugins/withoutVersionKey'
+import withoutKeys from '../plugins/withoutKeys'
 
 export interface IUserLean {
   username: string
+  email: string
+  password: string
+
+  links: {
+    steam?: string
+    oculus?: string
+  }
+
+  admin: boolean
 }
 
 export type IUserModel = IUserLean & Document
 
 const schema: Schema = new Schema({
-  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, maxlength: 255 },
+  username: { type: String, required: true, unique: true, maxlength: 24 },
+
+  links: {
+    oculus: { type: String, default: undefined, maxlength: 16 },
+    steam: { type: String, default: undefined, maxlength: 17 },
+  },
+
+  admin: { type: Boolean, default: false },
 })
 
-schema.plugin(withoutVersionKey)
+schema.plugin(withoutKeys(['__v', 'email', 'password', 'admin'], true))
 const User = mongoose.model<IUserModel>('user', schema)
 export default User
