2df1533098c588c460e8865347c7b352c46bdae0
Jack Baron <jackmbaron@gmail.com>
2019-05-18 14:01:59 +0100

    use new pagination system in maps api

--- server/src/routes/maps.ts
+++ server/src/routes/maps.ts
@@ -1,42 +1,39 @@
 import Router from 'koa-router'
 import { RESULTS_PER_PAGE } from '../env'
 import Beatmap from '../mongo/models/Beatmap'
+import { paginate } from '../mongo/plugins/paginate'
 
 const router = new Router({
   prefix: '/maps',
 })
 
 router.get('/latest/:page?', async ctx => {
-  const page = (Math.max(0, Number.parseInt(ctx.params.page, 10)) || 0) + 1
-  const { docs: maps, totalPages } = await Beatmap.paginate(
-    {},
-    { page, limit: RESULTS_PER_PAGE, sort: '-uploaded' }
-  )
+  const page = Math.max(0, Number.parseInt(ctx.params.page, 10)) || 0
+  const maps = await paginate(Beatmap, {}, { page, sort: '-uploaded' })
 
-  const lastPage = (totalPages as number) - 1
-  return (ctx.body = { maps, lastPage })
+  return (ctx.body = { maps: maps.docs, ...maps, docs: undefined })
 })
 
 router.get('/downloads/:page?', async ctx => {
-  const page = (Math.max(0, Number.parseInt(ctx.params.page, 10)) || 0) + 1
-  const { docs: maps, totalPages } = await Beatmap.paginate(
+  const page = Math.max(0, Number.parseInt(ctx.params.page, 10)) || 0
+  const maps = await paginate(
+    Beatmap,
     {},
-    { page, limit: RESULTS_PER_PAGE, sort: '-stats.downloads -uploaded' }
+    { page, sort: '-stats.downloads -uploaded' }
   )
 
-  const lastPage = (totalPages as number) - 1
-  return (ctx.body = { maps, lastPage })
+  return (ctx.body = { maps: maps.docs, ...maps, docs: undefined })
 })
 
 router.get('/plays/:page?', async ctx => {
-  const page = (Math.max(0, Number.parseInt(ctx.params.page, 10)) || 0) + 1
-  const { docs: maps, totalPages } = await Beatmap.paginate(
+  const page = Math.max(0, Number.parseInt(ctx.params.page, 10)) || 0
+  const maps = await paginate(
+    Beatmap,
     {},
-    { page, limit: RESULTS_PER_PAGE, sort: '-stats.plays -uploaded' }
+    { page, sort: '-stats.plays -uploaded' }
   )
 
-  const lastPage = (totalPages as number) - 1
-  return (ctx.body = { maps, lastPage })
+  return (ctx.body = { maps: maps.docs, ...maps, docs: undefined })
 })
 
 export { router as mapsRouter }
