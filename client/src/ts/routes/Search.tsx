c9210d00ed3f453349232ed8fba8f60364ea4163
Jack Baron <jackmbaron@gmail.com>
2019-05-19 12:50:23 +0100

    persist search query

--- client/src/ts/routes/Search.tsx
+++ client/src/ts/routes/Search.tsx
@@ -1,16 +1,36 @@
-import React, { FunctionComponent, useState } from 'react'
+import { push as pushFn } from 'connected-react-router'
+import { parse, stringify } from 'query-string'
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import { connect, MapStateToProps } from 'react-redux'
 import { BeatmapList } from '../components/Beatmap'
 import { Input } from '../components/Input'
+import { IState } from '../store'
 
-const Search: FunctionComponent = () => {
+interface IProps {
+  push: typeof pushFn
+  queryStr: string
+}
+
+const Search: FunctionComponent<IProps> = ({ push, queryStr }) => {
   const [query, setQuery] = useState('')
+  useEffect(() => {
+    const { q } = parse(queryStr)
+    if (q && typeof q === 'string') setQuery(q)
+  }, [])
+
+  const search = (q: string) => {
+    setQuery(q)
+
+    if (!q) push({ search: '' })
+    else push({ search: stringify({ q }) })
+  }
 
   return (
     <>
       <div className='thin' style={{ marginBottom: '24px' }}>
         <Input
           value={query}
-          onChange={v => setQuery(v)}
+          onChange={v => search(v)}
           placeholder='Search BeatSaver'
           iconClass='fas fa-search'
         />
@@ -21,4 +41,14 @@ const Search: FunctionComponent = () => {
   )
 }
 
-export default Search
+const mapStateToProps: MapStateToProps<IProps, {}, IState> = state => ({
+  push: pushFn,
+  queryStr: state.router.location.search,
+})
+
+const ConnectedSearch = connect(
+  mapStateToProps,
+  { push: pushFn }
+)(Search)
+
+export default ConnectedSearch
