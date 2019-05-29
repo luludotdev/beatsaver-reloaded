d71f679b7039aa4a24bcd09c2d4990d5a8775452
Jack Baron <jackmbaron@gmail.com>
2019-05-29 02:53:54 +0100

    enable local CDN for development

---
+++ server/src/routes/cdn.ts
@@ -0,0 +1,10 @@
+import Router from 'koa-router'
+import { Serve as serve } from 'static-koa-router'
+import { CDN_PATH } from '../constants'
+
+const router = new Router({
+  prefix: '/cdn',
+})
+
+serve(CDN_PATH, router)
+export { router as cdnRouter }
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,8 @@
 import Router from 'koa-router'
+import { IS_DEV } from '../env'
+import signale from '../utils/signale'
 import { authRouter } from './auth'
+import { cdnRouter } from './cdn'
 import { mapsRouter } from './maps'
 import { searchRouter } from './search'
 import { uploadRouter } from './upload'
@@ -10,3 +13,8 @@ export const routes: Router[] = [
   searchRouter,
   uploadRouter,
 ]
+
+if (IS_DEV) {
+  signale.warn('Enabling local CDN... Do not use this in production!')
+  routes.push(cdnRouter)
+}
