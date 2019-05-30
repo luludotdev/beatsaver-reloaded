3070c2cb765ae31a6bae9226d8beadbd5ea89e3c
Jack Baron <jackmbaron@gmail.com>
2019-05-30 02:52:28 +0100

    local strategy 404s when user not found

--- server/src/strategies/local.ts
+++ server/src/strategies/local.ts
@@ -2,12 +2,20 @@ import { compare } from 'bcrypt'
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
 import User from '../mongo/models/User'
+import CodedError from '../utils/CodedError'
+
+const ERR_INVALID_USERNAME = new CodedError(
+  'invalid username',
+  0x40001,
+  'ERR_INVALID_USERNAME',
+  404
+)
 
 passport.use(
   new LocalStrategy(async (username, password, cb) => {
     try {
       const user = await User.findOne({ username })
-      if (!user) return cb(null, false)
+      if (!user) return cb(ERR_INVALID_USERNAME, false)
 
       const valid = await compare(password, user.password)
       if (!valid) return cb(null, false)
