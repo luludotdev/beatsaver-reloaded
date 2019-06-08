61ef08bf2e5bfd598530ef8d9078ebff0d93e456
Jack Baron <jackmbaron@gmail.com>
2019-06-08 09:29:36 +0100

    add rate limit middleware

---
+++ server/src/middleware/ratelimit.ts
@@ -0,0 +1,65 @@
+import { Context } from 'koa'
+import koaRateLimit, {
+  HeaderNameOptions,
+  MiddlewareOptions,
+} from 'koa-ratelimit'
+import { rateLimitDB } from '../redis'
+
+interface IOptions {
+  /**
+   * The length of a single limiting period. This value is expressed
+   * in milliseconds, defaulting to one hour.
+   */
+  duration?: number
+
+  /**
+   * The maximum amount of requests a client (see the `id` field) may
+   * make during a limiting period. (see `duration`)
+   */
+  max?: number
+
+  /**
+   * Get the unique-identifier for a request. This defaults to the
+   * client's IP address. Returning "false" will skip rate-limiting.
+   */
+  id?: (context: Context) => string | false
+
+  /**
+   * Whether or not to disable the usage of rate limit headers. This defaults
+   * to **false**.
+   */
+  disableHeader?: boolean
+
+  /**
+   * The message used on the response body if a client is rate-limited. There is
+   * a default message; which includes when they should try again.
+   */
+  errorMessage?: string
+
+  /**
+   * Whether or not to throw an error upon being rate-limited. This uses
+   * the Koa context function "throw".
+   */
+  throw?: boolean
+
+  /**
+   * A relation of header to the header's display name.
+   */
+  headers?: HeaderNameOptions
+}
+
+export const rateLimit = (opts: IOptions) => {
+  const defaultOpts: Partial<IOptions> = {
+    headers: {
+      remaining: 'Rate-Limit-Remaining',
+      reset: 'Rate-Limit-Reset',
+      total: 'Rate-Limit-Total',
+    },
+    id: ctx => `${ctx.url}:${ctx.realIP}`,
+  }
+
+  const db = rateLimitDB as any
+  const options: MiddlewareOptions = { ...defaultOpts, ...opts, db }
+
+  return koaRateLimit(options)
+}
