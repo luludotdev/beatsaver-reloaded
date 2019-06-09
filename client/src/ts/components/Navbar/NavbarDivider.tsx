46dfc542697108e6845738854fe25e0eeee91c5f
Jack Baron <jackmbaron@gmail.com>
2019-06-09 09:36:53 +0100

    properly theme the navbar for mobile

--- client/src/ts/components/Navbar/NavbarDivider.tsx
+++ client/src/ts/components/Navbar/NavbarDivider.tsx
@@ -1,7 +1,5 @@
 import React, { FunctionComponent } from 'react'
 
 export const NavbarDivider: FunctionComponent = () => (
-  <div className='navbar-item' style={{ userSelect: 'none' }}>
-    |
-  </div>
+  <div className='navbar-item navbar-divi' />
 )
