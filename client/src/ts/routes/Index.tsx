ce1f06ae13f730afc86af78947325b3b252a67e2
Jack Baron <jackmbaron@gmail.com>
2019-06-10 19:23:20 +0100

    add notice about missing features

--- client/src/ts/routes/Index.tsx
+++ client/src/ts/routes/Index.tsx
@@ -1,8 +1,20 @@
 import React, { FunctionComponent } from 'react'
+import { ExtLink } from '../components/ExtLink'
 import { TextPage } from '../components/TextPage'
 
 export const Index: FunctionComponent = () => (
   <TextPage title='BeatSaver'>
     <p>Beat Saber's #1 unofficial beatmap distributor!</p>
+    <hr />
+
+    <p>
+      <b>Not everything is finished yet with the rewrite, please be patient.</b>
+      <br />
+      See{' '}
+      <ExtLink href='https://github.com/lolPants/beatsaver-reloaded/issues/1'>
+        this GitHub issue
+      </ExtLink>{' '}
+      for more info.
+    </p>
   </TextPage>
 )
