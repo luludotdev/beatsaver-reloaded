e54b09bb3a45026067d94e326b08992ad47ddbd6
Jack Baron <jackmbaron@gmail.com>
2019-06-11 03:19:37 +0100

    typo

--- client/src/ts/components/APIError.tsx
+++ client/src/ts/components/APIError.tsx
@@ -11,7 +11,7 @@ export const APIError: FunctionComponent<IProps> = ({ error }) => {
   return (
     <TextPage title='Error Loading Maps!'>
       <p>Something went wrong, please check back later.</p>
-      <p>If the problem persists please alert a side adminstrator.</p>
+      <p>If the problem persists please alert a site adminstrator.</p>
     </TextPage>
   )
 }
