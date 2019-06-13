39b9f7be289609625ea291a4e1df536a54b94c79
Jack Baron <jackmbaron@gmail.com>
2019-06-13 12:00:10 +0100

    use precalculated rating value for sorting

--- server/src/routes/maps.ts
+++ server/src/routes/maps.ts
@@ -47,119 +47,13 @@ router.get('/plays/:page?', mapCache, async ctx => {
 
 router.get('/hot/:page?', mapCache, async ctx => {
   const page = Math.max(0, Number.parseInt(ctx.params.page, 10)) || 0
+  const maps = await paginate(
+    Beatmap,
+    { deletedAt: null },
+    { page, sort: '-stats.heat', populate: 'uploader' }
+  )
 
-  interface IResult {
-    _id: string
-    score: number
-  }
-
-  // Reddit Hot Algorithm
-  const IDs: IResult[] = await Beatmap.aggregate([
-    { $match: { deletedAt: null } },
-    {
-      $project: {
-        _id: '$_id',
-        downVotes: {
-          $filter: {
-            as: 'vote',
-            cond: { $eq: ['$$vote.direction', -1] },
-            input: '$votes',
-          },
-        },
-        seconds: {
-          $subtract: [
-            {
-              $divide: [
-                { $subtract: ['$uploaded', new Date(Date.UTC(1970, 0, 1))] },
-                1000,
-              ],
-            },
-            1525132800,
-          ],
-        },
-        upVotes: {
-          $filter: {
-            as: 'vote',
-            cond: { $eq: ['$$vote.direction', 1] },
-            input: '$votes',
-          },
-        },
-      },
-    },
-    {
-      $project: {
-        _id: '$_id',
-        downVotes: { $size: '$downVotes' },
-        seconds: '$seconds',
-        upVotes: { $size: '$upVotes' },
-      },
-    },
-    {
-      $project: {
-        _id: '$_id',
-        score: { $subtract: ['$upVotes', '$downVotes'] },
-        seconds: '$seconds',
-      },
-    },
-    {
-      $project: {
-        _id: '$_id',
-        absolute: { $abs: '$score' },
-        score: '$score',
-        seconds: '$seconds',
-        sign: {
-          $cond: {
-            else: {
-              $cond: { if: { $lt: ['$score', 0] }, then: -1, else: 0 },
-            },
-            if: { $gt: ['$score', 0] },
-            then: 1,
-          },
-        },
-      },
-    },
-    {
-      $project: {
-        _id: '$_id',
-        order: { $log10: { $max: ['$absolute', 1] } },
-        seconds: '$seconds',
-        sign: '$sign',
-      },
-    },
-    {
-      $project: {
-        score: {
-          $add: [
-            { $multiply: ['$sign', '$order'] },
-            { $divide: ['$seconds', 45000] },
-          ],
-        },
-      },
-    },
-    { $sort: { score: -1 } },
-  ])
-
-  const totalDocs = IDs.length
-  const objectIDs = chunk(IDs, RESULTS_PER_PAGE)
-
-  const lastPage = objectIDs.length - 1
-  const prevPage = page - 1 === -1 ? null : page - 1
-  const nextPage = page + 1 > lastPage ? null : page + 1
-
-  const currentPage = objectIDs[page] || []
-  const maps = await Beatmap.find({ _id: { $in: currentPage } })
-  const docs = maps
-    .map(map => {
-      const result = IDs.find(x => x._id.toString() === map._id.toString())
-      const score = (result && result.score) || 0
-
-      return { map, score }
-    })
-    .sort((a, b) => b.score - a.score)
-    .map(({ map }) => map)
-
-  await Promise.all(docs.map(d => d.populate('uploader').execPopulate()))
-  return (ctx.body = { docs, totalDocs, lastPage, prevPage, nextPage })
+  return (ctx.body = maps)
 })
 
 router.get(
