329311df1675ea19e10ae7d04e95f433a6faef14
Jack Baron <jackmbaron@gmail.com>
2019-06-14 14:24:45 +0100

    allow NavbarItem to hook onClick

--- client/src/ts/components/Navbar/NavbarItem.tsx
+++ client/src/ts/components/Navbar/NavbarItem.tsx
@@ -10,15 +10,25 @@ import { ExtLink } from '../ExtLink'
 interface IItemProps {
   to: string
 
+  onClick?: (e: MouseEvent<HTMLAnchorElement>) => any
   setActive: Dispatch<SetStateAction<boolean>>
 }
 
 export const NavbarItem: FunctionComponent<IItemProps> = ({
   to,
   children,
+
+  onClick,
   setActive,
 }) => (
-  <Link className='navbar-item' to={to} onClick={() => setActive(false)}>
+  <Link
+    className='navbar-item'
+    to={to}
+    onClick={e => {
+      setActive(false)
+      if (typeof onClick === 'function') onClick(e)
+    }}
+  >
     {children}
   </Link>
 )
