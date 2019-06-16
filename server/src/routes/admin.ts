6b34839b9bf2b6c033302a4b0aa9873ffd989f7d
Jack Baron <jackmbaron@gmail.com>
2019-06-16 10:26:59 +0100

    secure the rest of the admin routes whoops

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -8,14 +8,18 @@ const router = new Router({
   prefix: '/admin',
 })
 
+router.use(passport.authenticate('jwt', { session: false }))
+router.use(async (ctx, next) => {
+  const user: IUserModel = ctx.state.user
+  if (!user.admin) return (ctx.status = 401)
+
+  await next()
+})
+
 router.post(
   '/migrate-votes',
-  passport.authenticate('jwt', { session: false }),
   ctx =>
     new Promise((resolve, reject) => {
-      const user: IUserModel = ctx.state.user
-      if (!user.admin) return resolve((ctx.status = 401))
-
       Beatmap.find({ 'stats.upVotes': { $exists: false } })
         .cursor()
         .on('data', doc =>
