18812fb7a788270dec836479ccdc41e8bfd964bf
Jack Baron <jackmbaron@gmail.com>
2019-06-19 07:01:59 +0100

    add user dump

--- server/src/routes/dump.ts
+++ server/src/routes/dump.ts
@@ -1,6 +1,7 @@
 import Router from 'koa-router'
 import { rateLimit } from '../middleware/ratelimit'
 import Beatmap from '../mongo/models/Beatmap'
+import User from '../mongo/models/User'
 import { jsonStream } from '../utils/streams'
 
 const router = new Router({
@@ -11,6 +12,14 @@ router.get('/maps', rateLimit(1000 * 60 * 60 * 12, 10), async ctx => {
   const maps = Beatmap.find({}, '-votes')
     .sort({ uploaded: -1 })
     .populate('uploader')
+    .cursor()
+    .pipe(jsonStream())
+
+  return (ctx.body = maps)
+})
+
+router.get('/users', rateLimit(1000 * 60 * 60 * 12, 10), async ctx => {
+  const maps = User.find({}, '-password -email')
     .cursor()
     .pipe(jsonStream())
 
