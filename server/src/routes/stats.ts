dd2eafcd398f47e8b3e3cc829dca1fb496e287d1
Jack Baron <jackmbaron@gmail.com>
2019-07-10 14:26:59 +0100

    make stats endpoints return the right data

--- server/src/routes/stats.ts
+++ server/src/routes/stats.ts
@@ -2,6 +2,8 @@ import cors from '@koa/cors'
 import Router from 'koa-router'
 import { cache } from '../middleware/cache'
 import { rateLimit } from '../middleware/ratelimit'
+import Beatmap from '../mongo/models/Beatmap'
+import { parseKey } from '../utils/parseKey'
 
 const router = new Router({
   prefix: '/stats',
@@ -14,19 +16,48 @@ const router = new Router({
     })
   )
 
+const findMap = async <T extends object = any>(query: T) => {
+  const map = await Beatmap.findOne(
+    { ...query, deletedAt: null },
+    { stats: 1, _id: 1, hash: 1, key: 1 }
+  )
+
+  if (!map) return null
+  const transformed = {
+    _id: map._id,
+    hash: map.hash,
+    key: map.key,
+    stats: map.stats,
+  }
+
+  return transformed
+}
+
 router.get(
   '/key/:key',
   cache({ prefix: ctx => `stats:key:${ctx.params.key}:`, expire: 60 * 10 }),
   async ctx => {
-    return (ctx.status = 501)
+    const key = parseKey(ctx.params.key)
+    if (key === false) return (ctx.status = 404)
+
+    const map = await findMap({ key })
+    if (!map) return (ctx.status = 404)
+
+    return (ctx.body = map)
   }
 )
 
 router.get(
   '/by-hash/:hash',
-  cache({ prefix: ctx => `stats:hash:${ctx.params.key}:`, expire: 60 * 10 }),
+  cache({ prefix: ctx => `stats:hash:${ctx.params.hash}:`, expire: 60 * 10 }),
   async ctx => {
-    return (ctx.status = 501)
+    if (typeof ctx.params.hash !== 'string') return (ctx.status = 400)
+    const hash = ctx.params.hash.toLowerCase()
+
+    const map = await findMap({ hash })
+    if (!map) return (ctx.status = 404)
+
+    return (ctx.body = map)
   }
 )
 
