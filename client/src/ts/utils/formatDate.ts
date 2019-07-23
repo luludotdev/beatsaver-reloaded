872056a3ff9a2f9868e9a5180a477dfbc37af51e
Jack Baron <jackmbaron@gmail.com>
2019-07-23 14:37:13 +0100

    swap out time formatter for a simpler one

--- client/src/ts/utils/formatDate.ts
+++ client/src/ts/utils/formatDate.ts
@@ -1,15 +1,36 @@
 import dateFormat from 'dateformat'
-import TimeAgo from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en'
 import { SEVEN_DAYS } from '../constants'
 
-TimeAgo.addLocale(en)
-export const timeAgo = new TimeAgo('en-US')
+interface IInterval {
+  label: string
+  seconds: number
+}
+
+const intervals: readonly IInterval[] = [
+  { label: 'year', seconds: 60 * 60 * 24 * 365 },
+  { label: 'month', seconds: 60 * 60 * 24 * 31 },
+  { label: 'week', seconds: 60 * 60 * 24 * 7 },
+  { label: 'day', seconds: 60 * 60 * 24 },
+  { label: 'hour', seconds: 60 * 60 },
+  { label: 'minute', seconds: 60 },
+  { label: 'second', seconds: 0 },
+]
+
+const timeSince = (date: Date | string) => {
+  const d = new Date(date)
+
+  const seconds = Math.floor((Date.now() - d.getTime()) / 1000)
+  const interval = intervals.find(i => i.seconds < seconds)
+  if (interval === undefined) return d.toISOString()
+
+  const count = Math.floor(seconds / interval.seconds)
+  return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`
+}
 
 export const formatDate = (date: Date | string) => {
   const d = typeof date === 'string' ? new Date(date) : date
 
   return Date.now() - d.getTime() < SEVEN_DAYS
-    ? timeAgo.format(d)
+    ? timeSince(d)
     : dateFormat(d, 'yyyy/mm/dd')
 }
