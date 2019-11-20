f35fbdc8d1fda5baf78d0231eeff2c904c8f1484
Jack Baron <jackmbaron@gmail.com>
2019-11-20 10:52:24 +0000

    fix lint error

--- server/src/routes/upload/parseBeatmap.ts
+++ server/src/routes/upload/parseBeatmap.ts
@@ -157,7 +157,7 @@ export const parseBeatmap: (
       const data: IDifficultyJSON = JSON.parse(content)
 
       const bombs = data._notes.filter(note => note._type === 3).length || 0
-      const duration = Math.max(...data._notes.map(note => note._time)) || 0
+      const charDuration = Math.max(...data._notes.map(note => note._time)) || 0
 
       const length =
         infoJSON._beatsPerMinute === 0
@@ -165,7 +165,7 @@ export const parseBeatmap: (
           : (duration / infoJSON._beatsPerMinute) * 60
 
       return {
-        duration,
+        duration: charDuration,
         length: Math.floor(length) || 0,
         njs: diff._noteJumpMovementSpeed || 0,
         njsOffset: diff._noteJumpStartBeatOffset || 0,
